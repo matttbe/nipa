6ce1ecd219e264735692e9a91b9ae7e34f959c61
Matthieu Baerts <matttbe@kernel.org>
2026-08-24 17:37:49 +0200

    docker: new base on top of Fedora
    This base will be used in the following commits to be able to run the
    static tests on one side, and the selftests on the other.
    
    The Dockerfile is simple: it contains a few common dependences, a 'nipa'
    user with a git config and CCACHE configured.
    
    A few simple bash scripts are added to build and run the docker images
    easily:
    
      ./docker/build.sh        # build all images
      ./docker/<dir>/build.sh  # build a specific image
      ./docker/<dir>/run.sh    # get a prompt or execute a command from a
                               # specific docker image
    
    The build scripts can be used with INPUT_PUSH=1 to publish the images
    after having built them.
    
    Signed-off-by: Matthieu Baerts <matttbe@kernel.org>

---
+++ docker/common/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: GPL-2.0
+
+: "${INPUT_PREFIX:="ghcr.io/linux-netdev"}"
+: "${INPUT_TAG:="latest"}"
+
+SCRIPT_DIR=$(cd "$(dirname "${0}")" && pwd)
+DIR_NAME="$(basename "${SCRIPT_DIR}")"
+CCACHE_DIR="${SCRIPT_DIR}/.ccache"
+mkdir -p "${CCACHE_DIR}"
+ARGS=(
+	-v "${CCACHE_DIR}:/home/nipa/.ccache:rw"
+	-v "${PWD}:${PWD}:rw"
+	-w "${PWD}"
+	-u "${RUID:-$(id -u)}:${RGID:-$(id -g)}"
+	--group-add "$(grep "^kvm:" /etc/group | cut -d: -f3)"
+	--rm
+	-i
+	--privileged
+)
+test -t 1 && ARGS+=("-t")
+
+DOCKER_IMG="${INPUT_PREFIX}/nipa-${DIR_NAME}:${INPUT_TAG}"
+echo "Using ${DOCKER_IMG} image" >&2
+
+# $1: basedir
+add_git_worktree() {
+	local wt
+	if [ -f "${1}/.git" ]; then
+		wt="$(realpath "$(git -C "${1}" rev-parse --git-common-dir)")"
+		ARGS+=(-v "${wt}:${wt}:rw")
+	fi
+}
+
+add_git_worktree .
+# trying to be smart: mounting directories passed in argument
+for arg in "${@}"; do
+	if [ -d "${arg}" ]; then
+		d="$(realpath "${arg}")"
+		ARGS+=(-v "${d}:${d}:rw")
+		add_git_worktree "${arg}"
+	fi
+done
+
+docker run "${ARGS[@]}" "${DOCKER_IMG}" "${@:-bash}"
